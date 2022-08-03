0eba8dcaa22aac78d27f9f404e6488a0c4218078
Protsenko Kate <protsenko.kate.47@gmail.com>
2022-08-03 22:19:15 +0300

    delete old calendar

--- src/components/Main/Calendar/Context/GlobalContext.js
+++
@@ -1,9 +0,0 @@
-import { buildTimeValue } from '@testing-library/user-event/dist/utils';
-import React from 'react';
-
-const GlobalContext = React.createContext({
-  monthIndex: 0,
-  setMonthIndex: (index) => {}
-})
-
-export default GlobalContext;
